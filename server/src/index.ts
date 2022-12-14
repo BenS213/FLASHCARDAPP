d1eb9a00b86e41e1786698e804a1ed767847739c
BenS213 <57325318+BenS213@users.noreply.github.com>
2022-12-14 17:28:18 -0500

    refactor server routes. created controllers file.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,40 +1,23 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
-import Deck from './models/Deck';
 import cors from 'cors';
 import { config } from 'dotenv';
+import { createDeckController } from './controllers/createDeckController';
+import { getDecksController } from './controllers/getDecksController';
+import { deleteDeckController } from './controllers/deleteDeckController';
 config();
 
 const PORT = 5000;
 
 const app = express();
-
 app.use(cors({
     origin: '*',
 }))
 app.use(express.json());
 
-app.post('/decks', async (req: Request, res: Response) => {
-    const newDeck = new Deck({
-        title: req.body.title
-    });
-    const createdDeck = await newDeck.save();
-    res.json(createdDeck)
-});
-
-app.get('/decks', async ( req: Request, res: Response) => {
-    const decks = await Deck.find();
-    res.json(decks)
-});
-
-app.delete('/decks/:deckId', async ( req: Request, res: Response) => {
-    // find the deck requested
-    const deckId = req.params.deckId;
-    // remove the deck from the database
-    const deck = await Deck.findByIdAndDelete(deckId)
-    // return the deleted deck to the user
-    res.json(deck)
-})
+app.get('/decks', getDecksController);
+app.post('/decks', createDeckController);
+app.delete('/decks/:deckId', deleteDeckController);
 
 const db = mongoose.connect(process.env.MONGO_URL!)
 .then(() => {
