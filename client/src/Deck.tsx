685ad6ede66439e2be18361a922e5b42f7acc4b1
BenS213 <57325318+BenS213@users.noreply.github.com>
2022-12-14 18:59:11 -0500

    added ability to add cards

--- client/src/Deck.tsx
+++ client/src/Deck.tsx
@@ -1,4 +1,57 @@
-
-export default function Deck() {
-    return <>Hello World</>
-}
+    import React, { useEffect, useState } from "react";
+    import { Link } from "react-router-dom";
+    import { createCard } from "./api/createCard";
+    import { deleteDeck } from "./api/deleteDecks";
+    import { Routes, Route, useParams } from "react-router-dom"
+    import { getDecks, TDeck } from "./api/getDecks";
+    import "./App.css";
+    
+    export default function Deck() {
+    
+      const [cards, setCards] = useState<string[]>([]);
+      const [text, setText] = useState("");
+      const { deckId } = useParams();
+      
+      async function handleCreateCard(e: React.FormEvent) {
+          e.preventDefault();
+          const {cards: serverCards} = await createCard(deckId!, text);
+          setCards(serverCards);
+        setText("");
+    }
+    
+    // async function handleDeleteDeck(deckId: string) {
+    //     await deleteDeck(deckId);
+    //     setText(text.filter(deck => deck._id !== deckId))
+    //   }
+      
+    //   useEffect(() => {
+    //       async function fetchDecks() {
+    //           const newDecks = await getDecks();
+    //           setText(newDecks)
+    //     }
+    //     fetchDecks();
+    // }, []);
+    
+    return (
+        <div className="App">
+          <ul className="decks">
+            {cards.map((card) => (
+              <li key={card}>
+                {/* <button onClick={() => handleDeleteCard(card._id)}>X</button> */}
+                {card}
+                </li>))}
+          </ul>
+          <form onSubmit={handleCreateCard}>
+            <label htmlFor="card-text">Card</label>
+            <input
+              id="card-text"
+              value={text}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setText(e.target.value);
+              }}
+            />
+            <button>Create Deck</button>
+          </form>
+        </div>
+      );
+    }
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import { config } from 'dotenv';
 import { createDeckController } from './controllers/createDeckController';
 import { getDecksController } from './controllers/getDecksController';
 import { deleteDeckController } from './controllers/deleteDeckController';
+import { createCardForDeckController } from './controllers/createCardForDeckController';
 config();
 
 const PORT = 5000;
@@ -18,6 +19,7 @@ app.use(express.json());
 app.get('/decks', getDecksController);
 app.post('/decks', createDeckController);
 app.delete('/decks/:deckId', deleteDeckController);
+app.post('/decks/:deckId/cards', createCardForDeckController);
 
 const db = mongoose.connect(process.env.MONGO_URL!)
 .then(() => {
